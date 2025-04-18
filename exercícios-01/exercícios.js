f212f127f881d89869006cdbbd0fa28faf5401be
Isaque-Cyber-Mil <isaque472436q@gmail.com>
2025-04-17 21:07:16 -0300

    calculadera

---
+++ exercícios-01/exercícios.js
@@ -0,0 +1,21 @@
+function somar(){
+    let n1, n2, soma, diminui
+
+n1 = Number (prompt("primeiro número: "))
+n2 = Number (prompt("segundo número: "))
+
+soma = n1 + n2
+
+alert("soma: " + soma)
+
+}
+function diminui(){
+n1 = Number (prompt("primeiro número: "))
+n2 = Number (prompt("segundo número: "))
+
+diminui = n1 - n2
+
+alert("diminui: " + diminui)
+}
+
+
